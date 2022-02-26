967876fdd6700968b74b4e2e631df6a59bdae279
masoodahamed <masoodahamed@gmail.com>
2022-02-26 17:53:05 +0530

    whats app working in mac with whatsapp app

---
+++ pyautoguy_whatspp_sendmsg.py
@@ -0,0 +1,27 @@
+import os
+import pyautogui
+import time
+
+
+def wp():
+    os.system("open /Applications/Whatsapp.app")
+    # pyautogui.click(941,457, clicks =2); pyautogui.typewrite('Hello world \n')
+    # pyautogui.screenshot('tab1.png',region=(0,0,1697,153))
+    x,y= pyautogui.locateCenterOnScreen('screen.png', confidence=0.5)
+    pyautogui.click(x/2,y/2)
+    pyautogui.typewrite('MASOOD AHAMED\n')
+    print(x,y)
+
+
+    sendx,sendy= pyautogui.locateCenterOnScreen('send_msg.png', confidence=0.5)
+    pyautogui.click(sendx/2,sendy/2)
+    pyautogui.typewrite('This is test message\n')
+
+
+wp()
+
+# (left=3058, top=284, width=68, height=54)
+
+# pyautogui.screenshot('tab1.png', region=(left=3058, top=284, width=68, height=54))  
+
+
